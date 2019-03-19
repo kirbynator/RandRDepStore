368106858d7397102f4f150f06ed2cc3d7432e7b
Zachary Kirby <i.am.kirbynator@gmail.com>
2019-03-19 14:08:59 -0600

    first time kinda nervous

---
+++ app/models/depo.rb
@@ -0,0 +1,3 @@
+class Depo < ApplicationRecord
+  has_many :items
+end
---
+++ test/models/depo_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class DepoTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
